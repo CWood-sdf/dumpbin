f1d226141926f5098cb9a0017de85bb5dd9fcc09
CWood-sdf <woodchristophermichael@gmail.com>
2024-04-11 16:30:10 -0400

    sdflskj

--- yeet.cpp
+++ yeet.cpp
@@ -1,22 +1,18 @@
 #include <vector>
 #include <cmath>
 
-void populate(std::vector<double>& x, std::vector<double>& y) {
+void populate(double x[], double y[]) {
     // do yeet stuff
 }
-std::vector<double>
-multiply(std::vector<double> left, std::vector<double> right) {
-
-    return {};
+void multiply(double x[], double y[], double out[]) {
 }
 
-std::vector<double> square(std::vector<double> arr) {
-    return {};
+void square(double arr[], double out[]) {
 }
-double slope(std::vector<double> x, std::vector<double> y) {
+double slope(double x[], double y[]) {
     return 0.0;
 }
-double yInt(std::vector<double> x, std::vector<double> y) {
+double yInt(double x[], double y[]) {
     return 0.0;
 }
 
