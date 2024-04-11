543319b186b74d0e87b2e1b0b40d44cc633b5fe8
CWood-sdf <woodchristophermichael@gmail.com>
2024-04-11 16:57:33 -0400

    sldfkjsdf

--- yeet.cpp
+++ yeet.cpp
@@ -1,6 +1,38 @@
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
 void populate(double x[], double y[]) {
     // do yeet stuff
+    std::ifstream file = std::ifstream("lin_reg.txt");
+    std::string val = "";
+    int xIndex = 0;
+    while (val[0] != 'Y') {
+        file >> val;
+        std::cout << val << std::endl;
+        if (val[0] == 'X') {
+            continue;
+        }
+        if (val[0] == 'Y') {
+            break;
+        }
+        double v = atof(val.data());
+        x[xIndex] = v;
+        xIndex++;
+    }
+    int yIndex = 0;
+    while (val[0] != 'x') {
+        file >> val;
+        std::cout << val << std::endl;
+        if (val.size() == 0) {
+            break;
+        }
+        double v = atof(val.data());
+        y[yIndex] = v;
+        yIndex++;
+        if (yIndex == xIndex) {
+            break;
+        }
+    }
 }
 void multiply(double x[], double y[], double out[]) {
 }
@@ -23,6 +55,11 @@ int main() {
     }
 
     populate(x, y);
+
+    for (int i = 0; x[i] < 5; i++) {
+        std::cout << "[" << x[i] << ", " << y[i] << "], ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
